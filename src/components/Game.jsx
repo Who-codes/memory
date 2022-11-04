7fb58116792ba37aa5d6fff36e5b30d06d0ee98c
Sulav <sulavshrestha023@gmail.com>
2022-11-04 18:28:53 +0545

    game functionality added

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -7,32 +7,94 @@ import Deadpool from "../assets/deadpool.svg";
 import Logan from "../assets/logan.svg";
 import RoboCop from "../assets/robocop.svg";
 import SingleCard from "./SingleCard";
+import { Link } from "react-router-dom";
 
-const cardsImg = [Batman, Cyclops, Daredevil, Deadpool, Logan, RoboCop];
+const cardsImg = [
+  { src: Batman, matched: false },
+  { src: Cyclops, matched: false },
+  { src: Daredevil, matched: false },
+  { src: Deadpool, matched: false },
+  { src: Logan, matched: false },
+  { src: RoboCop, matched: false },
+];
 
 const Game = () => {
   const [cards, setCards] = useState([]);
+  const [firstChoice, setFirstChoice] = useState(null);
+  const [secondChoice, setSecondChoice] = useState(null);
+  const [isFlipped, setIsFlipped] = useState(false);
 
   useEffect(() => {
     shuffle();
   }, []);
 
+  useEffect(() => {
+    checkCards();
+  }, [firstChoice, secondChoice]);
+
   const shuffle = () => {
     const shuffledCards = [...cardsImg, ...cardsImg]
       .sort(() => 0.5 - Math.random())
       .map((card) => {
-        return <SingleCard card={card} key={Math.random()} />;
+        return { ...card, id: Math.random() };
       });
 
     setCards(shuffledCards);
+    resetTurn();
+  };
+
+  const handleChoice = (card) => {
+    firstChoice ? setSecondChoice(card) : setFirstChoice(card);
+  };
+
+  const checkCards = () => {
+    if (firstChoice && secondChoice) {
+      if (firstChoice.src === secondChoice.src) {
+        console.log("matched");
+        setCards((prevState) => {
+          return prevState.map((card) => {
+            if (firstChoice.src === card.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+      } else {
+        console.log("no matched");
+      }
+      setTimeout(() => {
+        resetTurn();
+      }, 1000);
+    }
+  };
+
+  const resetTurn = () => {
+    setFirstChoice(null);
+    setSecondChoice(null);
   };
 
   return (
     <div className="game">
-      {cards}
+      {cards.map((card) => {
+        return (
+          <SingleCard
+            card={card}
+            key={card.id}
+            handleChoice={handleChoice}
+            isFlipped={
+              card === firstChoice || card === secondChoice || card.matched
+            }
+          />
+        );
+      })}
       <div className="btn-container">
-        <div className="btn">Restart</div>
-        <div className="btn">Go back</div>
+        <div className="btn" onClick={shuffle}>
+          Restart
+        </div>
+        <div className="btn">
+          <Link to={"/"}>Go back</Link>
+        </div>
       </div>
     </div>
   );
--- src/components/SingleCard.jsx
+++ src/components/SingleCard.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import Cover from "../assets/react.svg";
 import "./SingleCard.css";
 
-const SingleCard = ({ card }) => {
+const SingleCard = ({ card, handleChoice, isFlipped }) => {
+  const handleClick = () => {
+    handleChoice(card);
+    console.log(isFlipped);
+  };
+
   return (
-    <div className="cards">
-      <img src={Cover} alt="back card" className="back" />
-      <img src={card} alt="front card" className="front" />
+    <div className="card" onClick={handleClick}>
+      <div className={isFlipped ? "flip" : "flipped flip"}>
+        <img src={card.src} alt="front card" className="front" />
+        <img src={Cover} alt="back card" className="back" />
+      </div>
     </div>
   );
 };
